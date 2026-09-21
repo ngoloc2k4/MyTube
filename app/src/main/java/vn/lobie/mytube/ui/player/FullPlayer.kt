89020dc83694f19d623ee8a4dddb0122238ed78a
ngoloc2k4 <ngoloc2k4@users.noreply.github.com>
2026-09-21 14:27:40 +0700

    fix(player): import slideInVertically and slideOutVertically in FullPlayer.kt

--- app/src/main/java/vn/lobie/mytube/ui/player/FullPlayer.kt
+++ app/src/main/java/vn/lobie/mytube/ui/player/FullPlayer.kt
@@ -11,6 +11,8 @@ import androidx.compose.animation.AnimatedVisibility
 import androidx.compose.animation.animateContentSize
 import androidx.compose.animation.fadeIn
 import androidx.compose.animation.fadeOut
+import androidx.compose.animation.slideInVertically
+import androidx.compose.animation.slideOutVertically
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.gestures.detectDragGestures
